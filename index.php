81ac6b279ca29b0119b8914d39585ac54a534f1f
Dementrum <dmi56756877@yandex.ru>
2023-09-15 01:10:32 +0900

    sendMediaGroup

--- index.php
+++ index.php
@@ -46,6 +46,13 @@ if ($text == '/help') {
         'photo' => \Telegram\Bot\FileUpload\InputFile::create(__DIR__ . '/img/4.png'),
         'caption' => 'Some photo',
     ]);
+    $res = $telegram->sendPhoto([
+        'chat_id' => $chat_id,
+//        'photo' => \Telegram\Bot\FileUpload\InputFile::create('https://loremflickr.com/720/340'),
+//        'photo' => 'AgACAgIAAxkDAAN0ZP7MvSzNVz5bL-gjHyLZKexUYukAAhnPMRsRsfhLRiWjL6mqyqMBAAMCAANtAAMwBA',
+        'photo' => \Telegram\Bot\FileUpload\InputFile::create(__DIR__ . '/img/1.jpg'),
+        'caption' => 'Some photo',
+    ]);
 } elseif ($text == 'doc'){
     $res = $telegram->sendDocument([
         'chat_id' => $chat_id,
@@ -54,6 +61,15 @@ if ($text == '/help') {
         'thumbnail' => \Telegram\Bot\FileUpload\InputFile::create(__DIR__ . '/img/file.png')
     ]);
     debug($res);
+} elseif ($text == 'group') {
+//    $telegram->sendMediaGroup([
+//        'chat_id' => $chat_id,
+//        'media' => json_encode([
+//            ['type' => 'photo', 'media' => 'https://tel-bots.ru/bots/1/img/1.jpg', 'caption' => "Photo 1"],
+//            ['type' => 'photo', 'media' => 'https://tel-bots.ru/bots/1/img/2.jpg', 'caption' => "Photo 2"],
+//            ['type' => 'photo', 'media' => 'https://tel-bots.ru/bots/1/img/3.jpg', 'caption' => "Photo 3"],
+//        ]),
+//    ]);
 } elseif (!empty($text)) {
     $telegram->sendMessage([
         'chat_id' => $chat_id,
