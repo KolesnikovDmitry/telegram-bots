99a958cdaea6469d0b2c443e9232be7475120c3d
Dementrum <dmi56756877@yandex.ru>
2023-09-27 01:27:03 +0900

    sinoptic get weather

---
+++ 2/config.php
@@ -0,0 +1,5 @@
+<?php
+const TOKEN = '6444135716:AAGBV78ZLk5aTLTGxnjEYUPlKH9_w0A7Y4U';
+const WEATHER_TOKEN = '29f72b1e8c55a19bf0b371defbb4ed88';
+
+$weather_url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lang=ru&appid=' . WEATHER_TOKEN;
