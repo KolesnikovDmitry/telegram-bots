b3e2536d922bd01fff51ccd34b3c0d8b772637d2
Dementrum <dmi56756877@yandex.ru>
2023-10-02 23:35:35 +0900

    merge

--- 2/config.php
+++ 2/config.php
@@ -1,5 +1,1 @@
 <?php
-const TOKEN = '6444135716:AAGBV78ZLk5aTLTGxnjEYUPlKH9_w0A7Y4U';
-const WEATHER_TOKEN = '29f72b1e8c55a19bf0b371defbb4ed88';
-
-$weather_url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lang=ru&appid=' . WEATHER_TOKEN;
